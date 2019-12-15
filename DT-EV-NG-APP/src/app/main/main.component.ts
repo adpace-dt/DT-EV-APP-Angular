7e2352a29052cb3bbb8f6ef4c3c17154c0f0e921
Aripace <adpace@hotmail.com>
2019-12-15 10:40:35 -0700

    Many updates to time remaining functionality. checking in to move back to laptop.

--- DT-EV-NG-APP/src/app/main/main.component.ts
+++ DT-EV-NG-APP/src/app/main/main.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import * as moment from 'moment';
-import {Subject, BehaviorSubject, Observable} from "rxjs";
-import {interval, timer} from "rxjs";
-import {take} from "rxjs/operators";
+import {Subject, BehaviorSubject, Observable} from 'rxjs';
+import {interval, timer} from 'rxjs';
+import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-main',
@@ -19,11 +19,14 @@ export class MainComponent implements OnInit {
   slotTwoChargerNumber: number = 1;
   slotThreeChargerNumber: number = 2;
   slotFourChargerNumber: number = null;
-  slotOneTimeLeft$: any = null;
-  slotTwoTimeLeft: number = 1;
-  slotThreeTimeLeft: number = 2;
-  slotFourTimeLeft: number = null;
-  currentTime: any = moment();
+  slotOneTimeLeft$ = new BehaviorSubject(null);
+  slotTwoTimeLeft$: string;
+  slotThreeTimeLeft$: string;
+  slotFourTimeLeft$: string;
+  slotOneStartTimestamp: any = null;
+  slotTwoStartTimestamp: any = null;
+  slotThreeStartTimestamp: any = null;
+  slotFourStartTimestamp: any = null;
 
   // below will be used when refactoring for API's
   // parkingSlotOneOccupied = false;
@@ -36,13 +39,15 @@ export class MainComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    timer(0, 1000)
+    /*timer(0, 1000)
       .subscribe(() => {
-        let time = moment();
-        let end = moment(this.currentTime).add(3, 'hours');
-        let duration = moment.duration(time.diff(end));
-        this.slotOneTimeLeft$ = Math.abs(duration.hours()) + ':' + Math.abs(duration.minutes()) + ':' + (Math.abs(duration.seconds()) < 10 ? '0' + Math.abs(duration.seconds()) : Math.abs(duration.seconds()));
-      });
+        const time = moment();
+        const end = moment(this.currentTime).add(3, 'hours');
+        const duration = moment.duration(time.diff(end));
+        this.slotOneTimeLeft$ = (Math.abs(duration.hours()) > 0 ? Math.abs(duration.hours()) + ':' : '')
+          + (Math.abs(duration.minutes()) > 0 ? Math.abs(duration.minutes()) + ':' : '')
+          + (Math.abs(duration.seconds()) < 10 ? '0' + Math.abs(duration.seconds()) : Math.abs(duration.seconds()));
+      });*/
   }
 
   resetPendingCharger() {
@@ -50,11 +55,85 @@ export class MainComponent implements OnInit {
     this.pendingChargerNumber = null;
   }
 
+  setStartTime(slot) {
+    switch (slot) {
+      case 1:
+        this.slotOneStartTimestamp = moment();
+        break;
+      case 2:
+        this.slotTwoStartTimestamp = moment();
+        break;
+      case 3:
+        this.slotThreeStartTimestamp = moment();
+        break;
+      case 4:
+        this.slotFourStartTimestamp = moment();
+        break;
+    }
+    this.setRemainingTime(slot);
+  }
+
+  setRemainingTime(slot) {
+    switch (slot) {
+      case 1:
+        timer(0, 1000)
+          .subscribe(() => {
+            const time = moment();
+            const end = moment(this.slotOneStartTimestamp).add(3, 'hours');
+            const duration = moment.duration(time.diff(end));
+            this.slotOneTimeLeft$.next(
+              (Math.abs(duration.hours()) > 0 ? Math.abs(duration.hours()) + ':' : '')
+              + (Math.abs(duration.minutes()) > 0 ? Math.abs(duration.minutes()) + ':' : '')
+              + (Math.abs(duration.seconds()) < 10 ? '0' + Math.abs(duration.seconds()) : Math.abs(duration.seconds()))
+            );
+          });
+        break;
+
+      case 2:
+        timer(0, 1000)
+          .subscribe(() => {
+            const time = moment();
+            const end = moment(this.slotTwoStartTimestamp).add(3, 'hours');
+            const duration = moment.duration(time.diff(end));
+            this.slotTwoTimeLeft$ = (Math.abs(duration.hours()) > 0 ? Math.abs(duration.hours()) + ':' : '')
+              + (Math.abs(duration.minutes()) > 0 ? Math.abs(duration.minutes()) + ':' : '')
+              + (Math.abs(duration.seconds()) < 10 ? '0' + Math.abs(duration.seconds()) : Math.abs(duration.seconds()));
+          });
+        break;
+
+      case 3:
+        timer(0, 1000)
+          .subscribe(() => {
+            const time = moment();
+            const end = moment(this.slotThreeStartTimestamp).add(3, 'hours');
+            const duration = moment.duration(time.diff(end));
+            this.slotThreeTimeLeft$ = (Math.abs(duration.hours()) > 0 ? Math.abs(duration.hours()) + ':' : '')
+              + (Math.abs(duration.minutes()) > 0 ? Math.abs(duration.minutes()) + ':' : '')
+              + (Math.abs(duration.seconds()) < 10 ? '0' + Math.abs(duration.seconds()) : Math.abs(duration.seconds()));
+          });
+        break;
+
+      case 4:
+        timer(0, 1000)
+          .subscribe(() => {
+            const time = moment();
+            const end = moment(this.slotFourStartTimestamp).add(3, 'hours');
+            const duration = moment.duration(time.diff(end));
+            this.slotFourTimeLeft$ = (Math.abs(duration.hours()) > 0 ? Math.abs(duration.hours()) + ':' : '')
+              + (Math.abs(duration.minutes()) > 0 ? Math.abs(duration.minutes()) + ':' : '')
+              + (Math.abs(duration.seconds()) < 10 ? '0' + Math.abs(duration.seconds()) : Math.abs(duration.seconds()));
+          });
+        break;
+    }
+
+  }
+
   setCharger(chargerNumber, slot) {
     if (chargerNumber && slot) {
       if (slot === 1) {
         if (this.slotOneChargerNumber === null) {
           this.slotOneChargerNumber = chargerNumber;
+          this.setStartTime(slot);
           if (this.slotTwoChargerNumber === chargerNumber) {
             this.slotTwoChargerNumber = null;
           }
@@ -65,13 +144,16 @@ export class MainComponent implements OnInit {
             this.slotFourChargerNumber = null;
           }
           chargerNumber === 1 ? this.chargerOneSlot = slot : this.chargerTwoSlot = slot;
-          this.resetPendingCharger()
-        } else console.error('slot not empty');
+          this.resetPendingCharger();
+        } else {
+          console.error('slot not empty');
+        }
       }
 
       if (slot === 2) {
         if (this.slotTwoChargerNumber === null) {
           this.slotTwoChargerNumber = chargerNumber;
+          this.setStartTime(slot);
           if (this.slotOneChargerNumber === chargerNumber) {
             this.slotOneChargerNumber = null;
           }
@@ -82,13 +164,16 @@ export class MainComponent implements OnInit {
             this.slotFourChargerNumber = null;
           }
           chargerNumber === 1 ? this.chargerOneSlot = slot : this.chargerTwoSlot = slot
-          this.resetPendingCharger()
-        } else console.error('slot not empty');
+          this.resetPendingCharger();
+        } else {
+          console.error('slot not empty');
+        }
       }
 
       if (slot === 3) {
         if (this.slotThreeChargerNumber === null) {
           this.slotThreeChargerNumber = chargerNumber;
+          this.setStartTime(slot);
           if (this.slotOneChargerNumber === chargerNumber) {
             this.slotOneChargerNumber = null;
           }
@@ -100,12 +185,15 @@ export class MainComponent implements OnInit {
           }
           chargerNumber === 1 ? this.chargerOneSlot = slot : this.chargerTwoSlot = slot
           this.resetPendingCharger()
-        } else console.error('slot not empty');
+        } else {
+          console.error('slot not empty');
+        }
       }
 
       if (slot === 4) {
         if (this.slotFourChargerNumber === null) {
           this.slotFourChargerNumber = chargerNumber;
+          this.setStartTime(slot);
           if (this.slotOneChargerNumber === chargerNumber) {
             this.slotOneChargerNumber = null;
           }
@@ -117,7 +205,9 @@ export class MainComponent implements OnInit {
           }
           chargerNumber === 1 ? this.chargerOneSlot = slot : this.chargerTwoSlot = slot
           this.resetPendingCharger()
-        } else console.error('slot not empty');
+        } else {
+          console.error('slot not empty');
+        }
       }
 
     }
@@ -127,15 +217,50 @@ export class MainComponent implements OnInit {
     if (this.pendingChargerSlot === null) {
       this.pendingChargerSlot = $event.slotNumber;
       this.pendingChargerNumber = $event.chargerNumber;
-    } else if (this.pendingChargerSlot === $event.slotNumber) { //if the user clicks the charger a second time
-      this.resetPendingCharger()
+    } else if (this.pendingChargerSlot === $event.slotNumber) { // if the user clicks the charger a second time
+      this.resetPendingCharger();
     } else if (this.pendingChargerSlot !== null && this.pendingChargerSlot !== $event.slotNumber) {
       this.pendingChargerSlot = $event.slotNumber;
       this.pendingChargerNumber = $event.chargerNumber;
     }
   }
 
-  parkingSpotClickHandler(slotNumber){
-    this.setCharger(this.pendingChargerNumber, slotNumber)
+  parkingSpotClickHandler(slotNumber) {
+    this.resetChargerStartTimestamp();
+    this.setCharger(this.pendingChargerNumber, slotNumber);
+  }
+
+  resetChargerStartTimestamp() {
+    if (this.pendingChargerNumber === 1) {
+      switch (this.chargerOneSlot) {
+        case 1:
+          this.slotOneStartTimestamp = null;
+          break;
+        case 2:
+          this.slotTwoStartTimestamp = null;
+          break;
+        case 3:
+          this.slotThreeStartTimestamp = null;
+          break;
+        case 4:
+          this.slotFourStartTimestamp = null;
+          break;
+      }
+    } else {
+      switch (this.chargerTwoSlot) {
+        case 1:
+          this.slotOneStartTimestamp = null;
+          break;
+        case 2:
+          this.slotTwoStartTimestamp = null;
+          break;
+        case 3:
+          this.slotThreeStartTimestamp = null;
+          break;
+        case 4:
+          this.slotFourStartTimestamp = null;
+          break;
+      }
+    }
   }
 }
--- DT-EV-NG-APP/src/app/parking-spot/parking-spot.component.ts
+++ DT-EV-NG-APP/src/app/parking-spot/parking-spot.component.ts
@@ -1,30 +1,60 @@
-import {Component, OnInit, Input, EventEmitter, Output} from '@angular/core';
+import {Component, OnInit, OnChanges, Input, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-parking-spot',
   templateUrl: './parking-spot.component.html',
   styleUrls: ['./parking-spot.component.scss']
 })
-export class ParkingSpotComponent implements OnInit {
+export class ParkingSpotComponent implements OnInit, OnChanges {
   @Input() spotNumber: number;
   @Input() pendingChargerSlot;
-  @Input() timeLeft;
+  @Input() timeLeft: string;
   @Output() parkingSpotClickOutput = new EventEmitter<number>();
 
   occupied = false;
+  topMessage;
 
   constructor() {
   }
 
   ngOnInit() {
-  }
+      if (this.timeLeft) {
+        this.topMessage = this.timeLeft.toString();
+      } else if (!this.timeLeft && !this.occupied) {
+        this.topMessage = 'Space Available';
+      } else {
+        this.topMessage = 'This spot is waiting to charge - timestamp';
+      }
+    }
+
+    ngOnChanges() {
+      if (this.timeLeft) {
+        this.topMessage = this.timeLeft.toString();
+      } else if (!this.timeLeft && !this.occupied) {
+        this.topMessage = 'Space Available';
+      } else {
+        this.topMessage = 'This spot is waiting to charge - timestamp';
+      }
+    }
+
+    setTopMessage() {
+    if (!this.occupied) {
+      this.topMessage = 'Space Available';
+    } else if (!this.timeLeft && !this.occupied) {
+      this.topMessage = 'This spot is waiting to charge - timestamp';
+      } else {
+        this.topMessage = 'display time left';
+      }
+    }
 
   clickEmitter() {
-    if (this.pendingChargerSlot === null) {
+    if (this.pendingChargerSlot === null) {// if a charger hasn't been selected yet, toggle occupied status
       this.occupied = !this.occupied;
+      this.setTopMessage();
     } else if (this.pendingChargerSlot) {
       this.occupied = true;
       this.parkingSpotClickOutput.emit(this.spotNumber);
+      this.setTopMessage();
     }
 
     /*let payload = {
